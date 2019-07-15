d2dfdea22b89c35f5c18027aa37a1de9d9c1562f
Guilherme <gui.mochila@gmail.com>
2019-07-15 14:30:02 +0200

    Inital commit with HomePage and few components

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomePage.scss';
+
+import Directory from '../../components/Directory';
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+}
+
+export default HomePage;
