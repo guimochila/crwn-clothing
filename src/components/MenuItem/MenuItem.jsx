2ecd6b79ca0d7086c0cce6c8693134732a83bdb6
Guilherme <gui.mochila@gmail.com>
2019-07-15 15:13:57 +0200

    Add Routing for the items menu

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+
 import './MenuItem.scss';
 
-function MenuItem({ title, imageUrl, size }) {
+function MenuItem({ title, imageUrl, size, linkUrl, history, match }) {
   return (
-    <div className={`menu-item ${size || ''}`}>
+    <div
+      className={`menu-item ${size || ''}`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
@@ -16,4 +21,4 @@ function MenuItem({ title, imageUrl, size }) {
   );
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
