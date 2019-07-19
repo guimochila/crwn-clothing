d85c47664d7327f2aca3d148650a05a932f036e2
Guilherme <gui.mochila@gmail.com>
2019-07-19 20:36:33 +0200

    Add redux-persist to persist state in the localStorage

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import './index.css';
 import App from './App';
 import configureStore from './store';
 
-const store = configureStore();
+const { store, persistor } = configureStore();
 
 const Root = () => (
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
@@ -8,11 +10,20 @@ const rootReducer = combineReducers({
   cart: cartReducer,
 });
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const rootReducerWithPersistor = persistReducer(persistConfig, rootReducer);
+
 export default function configureStore() {
   const middlewares = [logger];
   const middlewareEnhancer = applyMiddleware(...middlewares);
 
-  const store = createStore(rootReducer, middlewareEnhancer);
+  const store = createStore(rootReducerWithPersistor, middlewareEnhancer);
+  const persistor = persistStore(store);
 
-  return store;
+  return { store, persistor };
 }
