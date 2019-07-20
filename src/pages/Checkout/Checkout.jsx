4824b1593b243f50735c08194082bcc3959404a0
Guilherme <gui.mochila@gmail.com>
2019-07-20 19:01:43 +0200

    Total move to top right when not visible

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { useInView } from 'react-intersection-observer';
 
 import './Checkout.scss';
 import {
@@ -10,6 +11,8 @@ import {
 import CheckoutItem from '../../components/CheckoutItem';
 
 function Checkout({ cartItems, total }) {
+  const [ref, inView] = useInView();
+
   return (
     <div className="checkout-page">
       <div className="checkout-header">
@@ -32,7 +35,8 @@ function Checkout({ cartItems, total }) {
       {cartItems.map(cartItem => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
-      <div className="total">
+      <div ref={ref} />
+      <div className={`total ${inView ? '' : 'total-not-visible'}`}>
         <span>TOTAL: ${total}</span>
       </div>
     </div>
