2f7dcdd960b15c83c73966f09fb72a4dbd49911d
Guilherme <gui.mochila@gmail.com>
2019-07-22 15:56:20 +0200

    Add styled components to CollectionPreview

--- src/components/CollectionPreview/CollectionPreview.jsx
+++ src/components/CollectionPreview/CollectionPreview.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import './CollectionPreview.scss';
 
+import {
+  CollectionPreviewContainer,
+  Title,
+  PreviewContainer,
+} from './CollectionPreview.styles';
 import CollectionItem from '../CollectionItem';
 
 function CollectionPreview({ title, items }) {
   return (
-    <div className="collection-preview">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <div className="preview">
+    <CollectionPreviewContainer>
+      <Title>{title.toUpperCase()}</Title>
+      <PreviewContainer>
         {items
           .filter((_, index) => index < 4)
           .map(item => (
             <CollectionItem key={item.id} item={item} />
           ))}
-      </div>
-    </div>
+      </PreviewContainer>
+    </CollectionPreviewContainer>
   );
 }
 
