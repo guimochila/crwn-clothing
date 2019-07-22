38a0044f3e83adcaab9fc73aaac2c02e156b5786
Guilherme <gui.mochila@gmail.com>
2019-07-22 15:26:27 +0200

    Add styled components to CartItem

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import './CartItem.scss';
+import {
+  CartItemContainer,
+  CartItemImage,
+  ItemDetailsContainer,
+  ItemName,
+} from './CartItem.styles';
 
 function CartItem({ item: { imageUrl, price, name, quantity } }) {
   return (
-    <div className="cart-item">
-      <img src={imageUrl} alt={name} />
-      <div className="item-details">
-        <span className="name">{name}</span>
-        <span className="price">
+    <CartItemContainer>
+      <CartItemImage src={imageUrl} alt={name} />
+      <ItemDetailsContainer>
+        <ItemName>{name}</ItemName>
+        <span>
           {quantity} x ${price}
         </span>
-      </div>
-    </div>
+      </ItemDetailsContainer>
+    </CartItemContainer>
   );
 }
 
