9c1bc8623ef5ee9b3f1fedc762ed3c1a87257d5f
Guilherme <gui.mochila@gmail.com>
2019-07-22 15:18:49 +0200

    Add styled components to CartIcon

--- src/components/CartIcon/CartIcon.jsx
+++ src/components/CartIcon/CartIcon.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './CartIcon.scss';
+import {
+  CartIconContainer,
+  ShoppingIcon,
+  ItemCountContainer,
+} from './CartIcon.styles';
 import { toggleCartHidden } from '../../store/cart/cart.actions';
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { selectCartItemsCount } from '../../store/cart/cart.selectors';
 
 function CartIcon({ toggleCartHidden, itemCount }) {
   return (
-    <div className="cart-icon" onClick={toggleCartHidden}>
-      <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{itemCount}</span>
-    </div>
+    <CartIconContainer onClick={toggleCartHidden}>
+      <ShoppingIcon />
+      <ItemCountContainer>{itemCount}</ItemCountContainer>
+    </CartIconContainer>
   );
 }
 
