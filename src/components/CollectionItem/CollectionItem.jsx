d1e66ddccf70d3822bf6e53fa4c6b1e7296c1d65
Guilherme <gui.mochila@gmail.com>
2019-07-22 14:58:45 +0200

    Add styled components to CollectionItem

--- src/components/CollectionItem/CollectionItem.jsx
+++ src/components/CollectionItem/CollectionItem.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { addItem } from '../../store/cart/cart.actions';
-import './CollectionItem.scss';
-import CustomButton from '../CustomButtom';
+import {
+  CollectionItemContainer,
+  CollectionFooterContainer,
+  AddButton,
+  BackgroundImage,
+  NameContainer,
+  PriceContainer,
+} from './CollectionItem.styles';
 
 function CollectionItem({ item, addItem }) {
   const { name, price, imageUrl } = item;
 
   return (
-    <div className="collection-item">
-      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
-      <div className="collection-footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
-      <CustomButton inverted onClick={() => addItem(item)}>
+    <CollectionItemContainer>
+      <BackgroundImage className="image" imageUrl={imageUrl} />
+      <CollectionFooterContainer>
+        <NameContainer>{name}</NameContainer>
+        <PriceContainer>{price}</PriceContainer>
+      </CollectionFooterContainer>
+      <AddButton inverted onClick={() => addItem(item)}>
         Add to cart
-      </CustomButton>
-    </div>
+      </AddButton>
+    </CollectionItemContainer>
   );
 }
 
