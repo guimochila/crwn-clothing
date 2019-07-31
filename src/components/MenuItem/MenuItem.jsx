a5b9320968f9e8513ee37ddb286f816a7210021c
Guilherme <gui.mochila@gmail.com>
2019-07-31 15:57:05 +0200

    Add styled components and mobile responsive

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
-import './MenuItem.scss';
+import {
+  MenuItemContainer,
+  BackgroundImageContainer,
+  ContentContainer,
+  ContentTitle,
+  ContentSubtitle,
+} from './MenuItem.styles';
 
 function MenuItem({ title, imageUrl, size, linkUrl, history, match }) {
   return (
-    <div
-      className={`menu-item ${size || ''}`}
+    <MenuItemContainer
+      size={size}
       onClick={() => history.push(`${match.url}${linkUrl}`)}
     >
-      <div
+      <BackgroundImageContainer
         className="background-image"
-        style={{ backgroundImage: `url(${imageUrl})` }}
+        imageUrl={imageUrl}
       />
-      <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">SHOP NOW</span>
-      </div>
-    </div>
+      <ContentContainer className="content">
+        <ContentTitle>{title.toUpperCase()}</ContentTitle>
+        <ContentSubtitle>SHOP NOW</ContentSubtitle>
+      </ContentContainer>
+    </MenuItemContainer>
   );
 }
 
