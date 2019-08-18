21717f4444377734c29142b165ea4aa3c1b018dd
Guilherme <gui.mochila@gmail.com>
2019-08-18 18:42:02 +0200

    Remove unused import

--- src/components/Error/Error.test.js
+++ src/components/Error/Error.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 
 import Error from './Error';
 import { ErrorImageContainer } from './Error.styles';
