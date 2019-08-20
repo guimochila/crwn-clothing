69c08373f37b798752ebdc74a4fb3a9379d82c26
Guilherme <gui.mochila@gmail.com>
2019-08-20 18:40:08 +0200

    Remove unused import

--- src/components/MenuItem/MenuItem.test.js
+++ src/components/MenuItem/MenuItem.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { mount } from 'enzyme';
 
 import { MenuItem } from './MenuItem';
-import { tsThisType } from '@babel/types';
 
 describe('MenuItem component', () => {
   let wrapper;
